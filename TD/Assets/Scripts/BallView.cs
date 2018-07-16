3a79002a0a40ecdc05182eba079e19554583e287
Tzu Chi Lin <hs0602steven@gmail.com>
2018-07-16 20:27:24 +0800

    Add empty project

---
+++ TD/Assets/Scripts/BallView.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallView : MonoBehaviour {
+
+	public Vector3 startPoint;
+	private Rigidbody2D rb2d;
+	public float speed;
+
+	void Awake(){
+		startPoint=GetComponentInParent<Transform>().position;
+		this.transform.position=startPoint;
+		rb2d=this.GetComponent<Rigidbody2D>();
+	} 
+
+	void Follow(Vector3 pos){
+		this.transform.position=pos;
+	}
+	public void Shoot(){
+		this.rb2d.velocity = (this.transform.position-startPoint)*speed;
+	}
+	
+}
---
+++ TD/Assets/Scripts/TouchControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class TouchControl : MonoBehaviour, IBeginDragHandler , IDragHandler , IDropHandler  {
+
+	public Camera camera;
+
+	[System.Serializable]
+	public class PassVector : UnityEvent<Vector3>{};
+
+	[SerializeField]
+	// Use this for initialization
+	private PassVector onDown;
+	[SerializeField]
+	private PassVector onDrag;
+	[SerializeField]
+	private UnityEvent onUp;
+	
+
+	
+	public void OnBeginDrag(PointerEventData eventData) {
+		this.onDown.Invoke(camera.ScreenToWorldPoint( (Vector3)eventData.position)); 
+	}
+	public void OnDrag(PointerEventData eventData) {
+		this.onDrag.Invoke(camera.ScreenToWorldPoint( (Vector3)eventData.position)); 
+	}
+	public void OnDrop(PointerEventData eventData) {
+		this.onUp.Invoke(); 
+	}
+
+	
+}
